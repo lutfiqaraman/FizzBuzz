87fbd8443fe4282894b92501d4d91edd78a85c0f
lutfiqaraman <lutfiqaraman@gmail.com>
2018-02-14 22:50:08 +0200

    Done ...

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,21 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            foreach (var number in numbers)
+            {
+                if (number % 15 == 0)
+                    Console.WriteLine(number + " is a fizz buzz number\n");
+                else if (number % 3 == 0)
+                    Console.WriteLine(number + " is a fizz number\n");
+                else if (number % 5 == 0)
+                    Console.WriteLine(number + " is a buzz number\n");
+                else
+                    Console.WriteLine(number + " is a normal number\n");
+            }
+
+            Console.ReadKey();
         }
     }
 }
